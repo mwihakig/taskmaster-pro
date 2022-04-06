981c09cafab9ffa6039ecda4c46dd3f9b55c3c6a
haki <githiimwihaki@gmail.com>
2022-04-06 18:15:42 -0400

    first commit

---
+++ Users/githi/movies-tv-shows/develop/assets/env/config.js
@@ -0,0 +1,6 @@
+const movie_tv_api = ''
+const client_id = '220e00aceb36a78989e7480d18bab96d8e154439c0201ca481e39afc2287eba4'
+const client_secret = '220e00aceb36a78989e7480d18bab96d8e154439c0201ca481e39afc2287eba4'
+
+
+const watch_mode_api = '3l405EmQ0wHLuDQ9qJ8skmA3nIiMhLeqXvJWZNCW'
---
+++ Users/githi/movies-tv-shows/develop/assets/js/javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+    console.log('HELO TEAM')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
